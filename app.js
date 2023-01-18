0728059e7e42f64687e5c65db2af0d7939413c76
Peter Akorede <oyinloyepeter273@gmail.com>
2023-01-18 16:52:47 +0000

    My portfolio

---
+++ app.js
@@ -0,0 +1,17 @@
+new Typed('.typing', {
+    strings : ['Frontend Web','Web Developer'],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop :true
+});
+new Typed('.auto', {
+    strings : ["I'm Oyinloye Peter and Web Developer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
+new Typed('.para', {
+    strings : ["I'm responsible for building a product that meets both the client's needs and those of the customer or end user. I'm a programmer who takes a web design-which has been created by either a client or a design team turns it..... "],
+    typeSpeed : 50,
+    loop : false
+})
